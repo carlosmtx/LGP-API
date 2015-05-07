d6202a0698ac8c0b8e4beadd22b9b9e573f46654
carlosmtx <cmcmteixeira@gmail.com>
2015-05-07 17:51:42 +0100

    [Updated] Trackable

--- src/AppBundle/Document/Trackable.php
+++ src/AppBundle/Document/Trackable.php
@@ -6,6 +6,7 @@
  */
 namespace AppBundle\Document;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 /** @MongoDB\Document(repositoryClass="AppBundle\Repository\TrackableRepository") */
 class Trackable extends Document{
@@ -17,4 +18,16 @@ class Trackable extends Document{
     public $originalName;
     /**@MongoDB\String() */
     public $description;
+    /**@MongoDB\ReferenceMany(targetDocument="Scene")
+     *  @var ArrayCollection  $scenes*/
+    public $scenes;
+    /** @MongoDB\ReferenceOne(targetDocument="Channel") */
+    public $channel;
+
+    public function __construct(){
+        parent::__construct();
+        $this->scenes = new ArrayCollection();
+    }
+
+
 }
