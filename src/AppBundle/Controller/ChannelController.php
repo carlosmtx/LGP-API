cab1c1e068af7c726cffae7b7db5510ec98aeff3
carlosmtx <cmcmteixeira@gmail.com>
2015-05-07 17:48:21 +0100

    [Added] Channel

--- src/AppBundle/Controller/ChannelController.php
+++ src/AppBundle/Controller/ChannelController.php
@@ -7,24 +7,31 @@ use AppBundle\Event\Channel\ChannelCreationEvent;
 use AppBundle\Event\Channel\ChannelEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ChannelController extends Controller
 {
-    public function createChannelAction($cname)
+    public function createChannelAction(Request $request,$cname)
     {
         $dm =     $this->get('doctrine_mongodb')->getManager();
 
         $channel = new Channel();
         $channel->name = $cname;
+        $channel->description = $request->request->get('description','');
 
+        if(!$channel){
+            return new Response("The channel $cname was not found",400);
+        }
 
         $event = new ChannelCreationEvent($channel);
         $this->get('event_dispatcher')->dispatch(ChannelEvent::ChannelCreation,$event);
 
         $dm->persist($channel);
         $dm->flush();
-        $dir = $this->container->getParameter('upload_root_dir')."/$cname";
-        $this->get('file_system_provider')->mkdir($dir);
-        return new JsonResponse('OK');
+
+        return new JsonResponse($channel);
     }
+
+
 }
