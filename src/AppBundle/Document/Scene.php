b96b6f37d9c03a2710bc88950229a78e6b23449e
carlosmtx <cmcmteixeira@gmail.com>
2015-05-07 17:49:23 +0100

    [Added] Scene

--- src/AppBundle/Document/Scene.php
+++ src/AppBundle/Document/Scene.php
@@ -1,11 +1,36 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
  * User: carlos
  * Date: 02/05/2015
  * Time: 15:33
  */
+namespace AppBundle\Document;
 
-class Scene {
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
+
+/** @MongoDB\Document(repositoryClass="AppBundle\Repository\SceneRepository") */
+class Scene extends Document{
+    /** @MongoDB\String() */
+    public $name;
+    /** @MongoDB\String() */
+    public $rootFolder;
+    /**@MongoDB\String() */
+    public $originalName;
+    /** @MongoDB\String() */
+    public $confFilePath;
+    /**@MongoDB\String() */
+    public $description;
+    /**@MongoDB\ReferenceMany(targetDocument="Scene")
+     *  @var ArrayCollection  $trackables */
+    public $trackables;
+    /** @MongoDB\ReferenceOne(targetDocument="Channel") */
+    public $channel;
+
+
+    public function __construct(){
+        parent::__construct();
+        $this->trackables = new ArrayCollection();
+    }
 }
